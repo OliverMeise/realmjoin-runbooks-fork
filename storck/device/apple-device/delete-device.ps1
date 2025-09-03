2f75d3bea9c786da051866955c2fbcc58216538d
Oliver Meise <oliver.meise@de.storck.com>
2025-09-03 11:07:50 +0200

    Update permissions and delete-device script: add wildcard to AllowedRunbookPatterns and change script functionality to delete devices instead of retiring them

--- storck/device/apple-device/delete-device.ps1
+++ storck/device/apple-device/delete-device.ps1
@@ -1,10 +1,9 @@
 <#
 .SYNOPSIS
-
-Retire a managed device from Intune (removes management and wipes company data) and delete it from Entra ID. (only iOS/iPadOS)
+Delete a managed device from Intune and delete it from Entra ID. (only iOS/iPadOS)
 
 .DESCRIPTION
-This script retires a managed device in Intune using Microsoft Graph. It verifies the device exists and then sends a retire command.
+Delete a managed device from Intune and delete it from Entra ID. (only iOS/iPadOS)
 
 .INPUTS
 RunbookCustomization: {
@@ -45,9 +44,10 @@ if ($null -eq $targetDevice) {
 "## OS: $($targetDevice.operatingSystem)"
 
 try {
-    Write-Output "## Retiring device $($targetDevice.displayName) with DeviceId $DeviceId."
-    Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/managedDevices/$($targetDevice.id)/retire" -Method "Post" -Beta | Out-Null
-    Write-Output "## Deleting $($targetDevice.deviceName) (Object ID $($targetDevice.id)) from Entra ID"
+    Write-Output "## Delete device $($targetDevice.displayName) (Object ID $($targetDevice.Id)) in Intune."
+    Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/managedDevices/$($targetDevice.id)" -Method "Delete" -Beta | Out-Null
+    
+    Write-Output "## Delete $($targetDevice.deviceName) (Object ID $($targetDevice.azureADDeviceId)) in Entra ID"
     Invoke-RjRbRestMethodGraph -Resource "/devices/$($DeviceId)" -Method Delete | Out-Null
 }
 catch {
